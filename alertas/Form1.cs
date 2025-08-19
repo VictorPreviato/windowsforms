a15afae61028e4ae32cd3723133bf44c679df17c
VICTOR GABRIEL PREVIATO <victor.gpreviato@senacsp.edu.br>
2025-08-19 11:36:58 -0300

    primeiro commit

---
+++ alertas/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace alertas
+{
+    public partial class Alertas : Form
+    {
+        public Alertas()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Alertas_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAlertaSimples_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Essa é uma mensagem simples", "Com um título");
+        }
+
+        private void btnAlertaRobusto_Click(object sender, EventArgs e)
+        {
+            DialogResult resposta = MessageBox.Show("Vamos programar um sistema?", 
+                                                    "Título aqui",
+                                                    MessageBoxButtons.YesNo,
+                                                    MessageBoxIcon.Question
+                                                    );
+                                                    
+            if(resposta == DialogResult.Yes)
+            {
+                MessageBox.Show("Respondeu que sim");
+            } else if (resposta == DialogResult.No)
+            {
+                MessageBox.Show("Repondeu que não");
+            }
+        }
+    }
+}
